48928d3f7398111d765ea996fdd0b1c545986bc2
Guillaume Knispel <xilun0@gmail.com>
2014-01-24 23:29:03 +0100

    fix RIPTV_FXS_001 and try reset in test_spi

--- test_spi/test_spi.c
+++ test_spi/test_spi.c
@@ -2,11 +2,47 @@
 #include <linux/errno.h>
 #include <linux/module.h>
 #include <linux/device.h>
+#include <linux/gpio.h>
+#include <linux/delay.h>
 
 #include <linux/spi/spi.h>
 
+int slic_le89156_reset(void)
+{
+	int gpio_reset = 20;       // GPIO for SLIC Reset
+
+	pr_info("%s: %d\n", __func__, gpio_reset);
+
+	gpio_free(gpio_reset);
+
+	if (gpio_request(gpio_reset, "TS_RST") != 0) {
+            pr_err("gpio_request() failed (%d)", gpio_reset);
+                return -1;
+	}
+
+	if( gpio_direction_output(gpio_reset, 1) != 0 )  { 
+		pr_err("gpio_direction_output() failed (%d)", gpio_reset);
+			return -1;
+	}
+
+	gpio_set_value(gpio_reset, 1); // Set GPIO TS_RST to OUTPUT
+	msleep(5);
+
+	gpio_set_value(gpio_reset, 0); // Set GPIO TS_RST to LOW
+	msleep(10);
+
+	gpio_set_value(gpio_reset, 1); // Set GPIO TS_RST to HIGH
+	msleep(5);
+
+	pr_info("slic reset\n");
+	gpio_free(gpio_reset);
+
+	return 0;
+}
+
 static int test_spi_probe(struct spi_device *spi)
 {
+	slic_le89156_reset();
 	dev_err(&spi->dev, "hello spi world\n");
 	spi_write(spi, "a", 1);
 	return 0;
